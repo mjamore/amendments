d7d49a28f8a60b72832a3fb987536ea998412050
Michael Amore <michael.amore@yahoo.com>
2015-05-23 23:44:12 -0400

    Fully working game. Like a boss.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # amendments
 A JavaScript game to test your knowledge of the U.S. Amendments
+
+
+To-Do:
+	- fix bug where random option is 'false'
+	- add ending to game when user completes all 27 questions
+	- add restart function when game is over
+	- refactor js
+	- fix overlay message on mobile screens
--- js/script.js
+++ js/script.js
@@ -1,143 +1,196 @@
 (function() {
 	$(function() {
-		console.log('working');
 
-		var model = [
+		var $answers = $('.answers');
+		var INITIAL_MODEL_LENGTH = model.length;
+		var totalCount = 0;
+		var currentScore = 0;
+
+		$('.total-amendments').text(INITIAL_MODEL_LENGTH);
+
+		/*
+		This will choose a random amendment from the model array. Call once per question
+		Returns an amendment object that consists of 'num', 'roman_num', and 'description'.
+		*/
+		var getRandomAmendment = function() {
+			var randomNum = Math.floor(Math.random()*model.length);
+			var randomAmendment = model[randomNum];
+			model.splice(randomNum, 1);
+			return randomAmendment;
+		}
+		// var amendmentObj = getRandomAmendment();
+		// console.log(amendmentObj);
+
+		/*
+		This will generate random numbers between 1 and 27. This function takes an integer, which is the number of random numbers you'd like returned.
+		Returns an array with the number of random numbers requested.
+		*/
+		var getRandomAmendmentNumbers = function(howManyRandoms) {
+			var randomNumbers = [];
+
+			var romanize = function(num) {
+			    if (!+num)
+			        return false;
+			    var digits = String(+num).split(""),
+			        key = ["","C","CC","CCC","CD","D","DC","DCC","DCCC","CM",
+			               "","X","XX","XXX","XL","L","LX","LXX","LXXX","XC",
+			               "","I","II","III","IV","V","VI","VII","VIII","IX"],
+			        roman = "",
+			        i = 3;
+			    while (i--)
+			        roman = (key[+digits.pop() + (i * 10)] || "") + roman;
+			    return Array(+digits.join("") + 1).join("M") + roman;
+			}
+
+			for(var i = 0; i < howManyRandoms; i++)
 			{
-				"num": 1,
-				"roman_num": "I",
-				"description": "Prohibits the making of any law respecting an establishment of religion, impeding the free exercise of religion, abridging the freedom of speech, infringing on the freedom of the press, interfering with the right to peaceably assemble or prohibiting the petitioning for a governmental redress of grievances."
-			},
+				var number = Math.floor(Math.random()*model.length);
+				randomNumbers.push(romanize(number));
+			}
+
+			return randomNumbers;
+		}
+		// var letter = getRandomAmendmentNumbers(3);
+		// console.log(letter);
+
+		/*
+		This will generate the correct answer. Call once per question
+		Returns either 'A', 'B', 'C', or 'D'
+		*/
+		var getRandomCorrectLetter = function() {
+			var randomNum = Math.floor(Math.random()*4);
+			var letter = "";
+			if(randomNum === 0) { letter = "A"; }
+			else if (randomNum === 1) { letter = "B"; }
+			else if (randomNum === 2) { letter = "C"; }
+			else if (randomNum === 3) { letter = "D"; }
+			return letter;
+		}
+
+
+
+		/*
+		This will generate the correct answer. Call once per question
+		Returns either 'A', 'B', 'C', or 'D'
+		*/
+		var generateQuestion = function() {
+			// init function for generating questions
+			// call other functions to generate questions parameters
+
+			var setCorrectValue = function() {
+				if(correctLetter === "A") { questionModel.A = amendmentObj.roman_num; }
+				else if (correctLetter === "B") { questionModel.B = amendmentObj.roman_num; }
+				else if (correctLetter === "C") { questionModel.C = amendmentObj.roman_num; }
+				else if (correctLetter === "D") { questionModel.D = amendmentObj.roman_num; }
+			}
+
+			var questionModel = {
+				"amendment_description" : "",
+				"correct_letter": "",
+				"A": "",
+				"B": "",
+				"C": "",
+				"D": ""
+			}
+
+			// Call once
+			var amendmentObj = getRandomAmendment();
+			questionModel.amendment_description = amendmentObj.description;
+
+			// Call once
+			var correctLetter = getRandomCorrectLetter();
+			questionModel.correct_letter = correctLetter;
+
+			setCorrectValue();
+
+			var incorrectAnswers = getRandomAmendmentNumbers(3);
+
+			
+			/*
+			This will set the other 3 answers to random values
+			*/
+			var setOtherLetters = function(incorrectAnswers) {
+				if( questionModel.A === "" )
+				{
+					questionModel.A = incorrectAnswers[incorrectAnswers.length - 1];
+					incorrectAnswers.pop();
+				}
+				if( questionModel.B === "" )
+				{
+					questionModel.B = incorrectAnswers[incorrectAnswers.length - 1];
+					incorrectAnswers.pop();
+				}
+				if( questionModel.C === "" )
+				{
+					questionModel.C = incorrectAnswers[incorrectAnswers.length - 1];
+					incorrectAnswers.pop();
+				}
+				if( questionModel.D === "" )
+				{
+					questionModel.D = incorrectAnswers[incorrectAnswers.length - 1];
+					incorrectAnswers.pop();
+				}
+			}
+			setOtherLetters(incorrectAnswers);
+
+			console.log(questionModel);
+			return questionModel;
+		}
+		questionModel = generateQuestion();
+
+		var renderQuestion = function(questionModel) {
+			var $amendmentDescription = $('.amendment-description'),
+				$a = $answers.find('.a span'),
+				$b = $answers.find('.b span'),
+				$c = $answers.find('.c span'),
+				$d = $answers.find('.d span');
+
+			$amendmentDescription.text(questionModel.amendment_description);
+			$a.text(questionModel.A);
+			$b.text(questionModel.B);
+			$c.text(questionModel.C);
+			$d.text(questionModel.D);
+
+			totalCount++;
+			$('.total-count').text(totalCount);
+			$('.current-question').text(totalCount);
+		}
+		renderQuestion(questionModel);
+
+
+
+
+		// Handle user answer selection logic
+		/*
+		* This function is called when a user selects an answer for each question
+		*/
+		var checkAnswer = function(value) {
+			if( value === questionModel.correct_letter )
 			{
-				"num": 2,
-				"roman_num": "II",
-				"description": "Protects the right to keep and bear arms."
-			},
-			{
-				"num": 3,
-				"roman_num": "III",
-				"description": "Places restrictions on the quartering of soldiers in private homes without the owner's consent, prohibiting it during peacetime."
-			},
-			{
-				"num": 4,
-				"roman_num": "IV",
-				"description": "Prohibits unreasonable searches and seizures and sets out requirements for search warrants based on probable cause as determined by a neutral judge or magistrate."
-			},
-			{
-				"num": 5,
-				"roman_num": "V",
-				"description": "Sets out rules for indictment by grand jury and eminent domain, protects the right to due process, and prohibits self-incrimination and double jeopardy."
-			},
-			{
-				"num": 6,
-				"roman_num": "VI",
-				"description": "Protects the right to a fair and speedy public trial by jury, including the rights to be notified of the accusations, to confront the accuser, to obtain witnesses and to retain counsel."
-			},
-			{
-				"num": 7,
-				"roman_num": "VII",
-				"description": "Provides for the right to trial by jury in certain civil cases, according to common law."
-			},
-			{
-				"num": 8,
-				"roman_num": "VIII",
-				"description": "Prohibits excessive fines and excessive bail, as well as cruel and unusual punishment."
-			},
-			{
-				"num": 9,
-				"roman_num": "IX",
-				"description": "Protects rights not enumerated in the Constitution."
-			},
-			{
-				"num": 10,
-				"roman_num": "X",
-				"description": "Reinforces the principle of federalism by stating that the federal government possesses only those powers delegated to it by the states or the people through the Constitution."
-			},
-			{
-				"num": 11,
-				"roman_num": "XI",
-				"description": "Makes states immune from suits from out-of-state citizens and foreigners not living within the state borders; lays the foundation for sovereign immunity."
-			},
-			{
-				"num": 12,
-				"roman_num": "XII",
-				"description": "Revises presidential election procedures."
-			},
-			{
-				"num": 13,
-				"roman_num": "XIII",
-				"description": "Abolishes slavery, and involuntary servitude, except as punishment for a crime."
-			},
-			{
-				"num": 14,
-				"roman_num": "XIV",
-				"description": "Defines citizenship, contains the Privileges or Immunities Clause, the Due Process Clause, the Equal Protection Clause, and deals with post-Civil War issues."
-			},
-			{
-				"num": 15,
-				"roman_num": "XV",
-				"description": "Prohibits the denial of the right to vote based on race, color, or previous condition of servitude."
-			},
-			{
-				"num": 16,
-				"roman_num": "XVI",
-				"description": "Permits Congress to levy an income tax without apportioning it among the states or basing it on the United States Census."
-			},
-			{
-				"num": 17,
-				"roman_num": "XVII",
-				"description": "Establishes the direct election of United States Senators by popular vote."
-			},
-			{
-				"num": 18,
-				"roman_num": "XVIII",
-				"description": "Prohibited the manufacturing or sale of alcohol within the United States."
-			},
-			{
-				"num": 19,
-				"roman_num": "XIX",
-				"description": "Prohibits the denial of the right to vote based on sex."
-			},
-			{
-				"num": 20,
-				"roman_num": "XX",
-				"description": "Changes the date on which the terms of the President and Vice President (January 20) and Senators and Representatives (January 3) end and begin."
-			},
-			{
-				"num": 21,
-				"roman_num": "XXI",
-				"description": "Repeals the 18th Amendment and prohibits the transportation or importation into the United States of alcohol for delivery or use in violation of applicable laws."
-			},
-			{
-				"num": 22,
-				"roman_num": "XXII",
-				"description": "Limits the number of times that a person can be elected president: a person cannot be elected president more than twice, and a person who has served more than two years of a term to which someone else was elected cannot be elected more than once."
-			},
-			{
-				"num": 23,
-				"roman_num": "XXIII",
-				"description": "Grants the District of Columbia electors (the number of electors being equal to the least populous state) in the Electoral College."
-			},
-			{
-				"num": 24,
-				"roman_num": "XXIV",
-				"description": "Prohibits the revocation of voting rights due to the non-payment of a poll tax."
-			},
-			{
-				"num": 25,
-				"roman_num": "XXV",
-				"description": "Addresses succession to the Presidency and establishes procedures both for filling a vacancy in the office of the Vice President, as well as responding to Presidential disabilities."
-			},
-			{
-				"num": 26,
-				"roman_num": "XXVI",
-				"description": "Prohibits the denial of the right of US citizens, eighteen years of age or older, to vote on account of age."
-			},
-			{
-				"num": 27,
-				"roman_num": "XXVII",
-				"description": "Delays laws affecting Congressional salary from taking effect until after the next election of representatives."
-			},
-		];
+				$('.message').removeClass('incorrect').addClass('correct').text('Correct!');
+				currentScore++;
+			}
+			else {
+				$('.message').removeClass('correct').addClass('incorrect').text('Incorrect!');
+			}
+
+			$('.overlay').fadeIn();
+			setTimeout(function() {
+				$('.overlay').fadeOut();
+				setTimeout(function() {
+					questionModel = generateQuestion();
+					renderQuestion(questionModel);
+					$('.current-score').text(currentScore);
+				}, 500);
+			}, 2000);
+		}
+
+		$answers.on('click', 'a', function() {
+			var value = $(this).text().charAt(0);
+			checkAnswer(value);
+		});
+
+
+		
 	});
 })();
